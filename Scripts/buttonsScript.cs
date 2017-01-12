5ffabdee36a68a81c59e8d0ec1e408d73c91c37f
Troller235 <alexeyua79@gmail.com>
2017-01-12 12:05:37 -0500

    Button and Splash
    Button and Splash scripts below...

---
+++ Scripts/buttonsScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonsScript : MonoBehaviour
+{
+    public void newGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+    public void exitGameBtn()
+    {
+        Application.Quit();
+    }
+}
